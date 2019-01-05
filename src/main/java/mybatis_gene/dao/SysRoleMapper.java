a42e123228cb3ff30493f429305a741ad6c51f65
githubwwp <wwp231@163.com>
2019-01-05 19:54:42 +0800

    demo

---
+++ src/main/java/mybatis_gene/dao/SysRoleMapper.java
@@ -0,0 +1,17 @@
+package mybatis_gene.dao;
+
+import mybatis_gene.entity.SysRole;
+
+public interface SysRoleMapper {
+    int deleteByPrimaryKey(String roleCode);
+
+    int insert(SysRole record);
+
+    int insertSelective(SysRole record);
+
+    SysRole selectByPrimaryKey(String roleCode);
+
+    int updateByPrimaryKeySelective(SysRole record);
+
+    int updateByPrimaryKey(SysRole record);
+}
